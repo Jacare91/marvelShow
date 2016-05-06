cf8b4589c4971395c1b91cacac4da8bb170fbe98
Tomek Spędzia <tomasz.spedzia@comarch.com>
2016-05-06 14:49:01 +0200

    Initial commit!

---
+++ app/src/main/java/com/jacare/marvelshow/MainActivity.java
@@ -0,0 +1,26 @@
+package com.jacare.marvelshow;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.preview_screen)protected WebView previewScreen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        previewScreen.loadUrl("https://marvelapp.com/jdfej6");
+    }
+
+}
